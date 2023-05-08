308db1bde72b88ab73a3c8669c6869871a0a2c7a
Fahad <76650975+mediocre9@users.noreply.github.com>
2023-05-08 07:43:29 +0500

    refactor: database class

---
+++ Config/Config.cs
@@ -0,0 +1,21 @@
+﻿namespace LibraryCMS.Config
+{
+    /// <summary>
+    /// Database environment variables
+    /// </summary>
+    public class Config
+    {
+        private Config() { }
+
+        private static readonly string HOST = "localhost";
+        private static readonly string USER = "root";
+        private static readonly string DATABASE = "library_cms";
+        private static readonly string PASSWORD = "";
+
+        public static readonly string ADDRESS =
+            $"Server     = {HOST}    ;" +
+            $"Database   = {DATABASE};" +
+            $"Uid        = {USER}    ;" +
+            $"Pwd        = {PASSWORD}";
+    }
+}
---
+++ Services/DBConnection.cs
@@ -0,0 +1,38 @@
+﻿using MySql.Data.MySqlClient;
+using LibraryCMS.Config;
+
+namespace Services
+{
+    /// <summary>
+    /// A singleton database connection to communicate 
+    /// with only single open instance.
+    /// </summary>
+    /// 
+    /// <remarks>
+    /// Use pool connections for database, if there are 
+    /// multiple connections.
+    /// </remarks>
+    public class DBConnection
+    {
+        /// <returns>
+        /// Returns a single <code>MySqlConnection</code> instance.
+        /// </returns>
+        public static MySqlConnection GetInstance()
+        {
+            try
+            {
+                instance = new MySqlConnection(Config.ADDRESS);
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+            }
+
+            return instance!;
+        }
+
+        private DBConnection() { }
+
+        private static MySqlConnection? instance;
+    }
+}
