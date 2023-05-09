ea4b9d6b9263d1c74e9d0994008b880c3cfa0258
Fahad <76650975+mediocre9@users.noreply.github.com>
2023-05-10 01:45:13 +0500

    chore: change resource files

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace _11939_Major_Agmnt_.Properties {
+namespace LibraryCMS.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace _11939_Major_Agmnt_.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("_11939_Major_Agmnt_.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LibraryCMS.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
